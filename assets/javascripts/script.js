f3e6ee3274fdbc925bfeb1f3f47a57da84d5960f
Abutalib <talibbca960@gmail.com>
2024-07-01 02:09:29 +0530

    new_change

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -87,3 +87,22 @@ function activeDeactive(params, params1) {
 }
 
 activeDeactive('content-1','side-header');
+
+$(document).ready(function() {
+  $(document).on('click', '.dropdown-toggle', function(event) {
+      event.preventDefault();
+
+      if ($(window).width() <= 768) {
+          var $dropdownMenu = $(this).siblings('.dropdown-menu');
+          $('.dropdown-menu').not($dropdownMenu).css('display', 'none !important');
+          $dropdownMenu.toggle();
+      }
+  });
+  $(document).on('click', function(event) {
+      if (!$(event.target).closest('.dropdown-toggle').length) {
+          $('.dropdown-menu').css('display', 'none');
+      }
+  });
+});
+
+
